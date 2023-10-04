ce3ad466152083399e98af34efc7952ec600d921
aitor-arronte <aitoraalvarez@gmail.com>
2023-10-04 13:33:26 -1000

    added main

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from song_collection import SongCollection
+import argparse
+import pandas as pd
+import json
+import os, requests
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--artists', type=str, default=None, help='path to txt file with a list of artists name to create dataset file.')
+    parser.add_argument('-d', '--download_previews', type=str, default=None, help='You will need to create first the songs.json file by '
+                                                                                  'executing the -a command')
+    parser.add_argument('-j', '--convert_to_excel', help='Convert songs.json file to excel')
 
+    args = parser.parse_args()
+    if args.artists:
+        df = pd.read_table(args.artists)
+        artists = df["artist"].to_list()
+        songs = SongCollection(artists)
+        dataset = songs.create_dataset()
+        json_file = json.loads(dataset)
+        with open('songs.json', 'w', encoding='utf-8') as f:
+            json.dump(json_file, f, ensure_ascii=False, indent=4)
+        print("The dataset has been created and saved to the file songs.json")
+    if args.download_previews:
+        if os.path.isfile('./songs.json'):
+            json_data = json.load('./songs.json')
+            if not os.path.exists('./songs/'):
+                os.makedirs('songs')
+                print("directory songs created")
+            for j in json_data['data']:
+                audio = requests.get(j['previewUrl'])
+                with open('songs/'+j['song_id']+".m4a", 'wb') as f:
+                    f.write(audio.content)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+        else:
+            print("The file songs.json is missing. Make sure the file exists with the songs that need to be downloaded. See -h for help")
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- song_collection.py
+++ song_collection.py
@@ -4,16 +4,13 @@ import pandas as pd
 
 class SongCollection:
 
-	def __init__(self, artists=None, songs=None ):
+	def __init__(self, artists):
 		self.search_url = 'https://itunes.apple.com/search?term='
 		self.artists = artists
-		self.songs = songs
 
 	def get_album_data(self, artist=None, song=None):
 		if artist is not None:
 			url = self.search_url+artist+"&entity=song"
-		elif song is not None:
-			url = self.search_url + song + "&entity=song"
 
 		try:
 			response_data = requests.get(url)
@@ -34,30 +31,45 @@ class SongCollection:
 		previewUrls=[]
 		primaryGenre=[]
 
-		if self.artists is not None:
-			keywords = self.artists
-		elif self.albums is not None:
-			keywords = self.albums
-		for term in keywords:
+		for term in self.artists:
 			data = self.get_album_data(term)
 
 			if data is not None:
 				for result in data['results']:
 					try:
-						artists.append(result['artistName'])
-						artists_id.append(result['artistId'])
-						songs.append(result['trackName'])
-						song_ids.append(result['trackId'])
-						previewUrls.append(result['previewUrls'])
-						primaryGenre.append(result['primaryGenreName'])
+						if result['artistName']:
+							artists.append(result['artistName'])
+						else:
+							artists.append("None")
+						if result['artistId']:
+							artists_id.append(result['artistId'])
+						else:
+							artists_id.append("None")
+						if result['trackName']:
+							songs.append(result['trackName'])
+						else:
+							songs.append(" ")
+						if result['trackId']:
+							song_ids.append(result['trackId'])
+						else:
+							song_ids.append(" None")
+						if result['previewUrl']:
+							previewUrls.append(result['previewUrl'])
+						else:
+							previewUrls.append("None ")
+						if result['primaryGenreName']:
+							primaryGenre.append(result['primaryGenreName'])
+						else:
+							primaryGenre.append("None")
 					except:
 						continue
 			else:
 				pass
 			sleep(4)
 
-		df = pd.DataFrame({'artist':artists, 'artist_id':artists_id, 'song':songs, 'song_id': song_ids, 'previewUrls':previewUrls, 'primaryGenre':primaryGenre})
-
-		return df
+		df = pd.DataFrame({'artist':artists, 'artist_id':artists_id, 'song':songs, 'song_id': song_ids, 'previewUrl':previewUrls, 'primaryGenre':primaryGenre})
+		df = df.reset_index(drop=True)
+		json_songs = df.to_json(orient="split")
+		return json_songs
 
 
